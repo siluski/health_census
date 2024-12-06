8ee938bfbd7ba94d7febe48c0a30fc19e0b76e57
siluski <arsiluk@optonline.net>
2024-12-06 14:29:30 -0500

    added

--- health_analysis.js
+++ health_analysis.js
@@ -4,6 +4,7 @@ const btnSearch = document.getElementById("btnSearch");
 const patients = [];
 
 addPatientButton.addEventListener("click", addPatient);
+btnSearch.addEventListener("click", searchCondition);
 
 function addPatient() {
     const name = document.getElementById("name").value;
@@ -23,6 +24,34 @@ function resetForm(){
     document.querySelector('input[name="gender"]:checked').checked = false;
     document.getElementById("age").value = '';
     document.getElementById('condition').value = '';
+}
+
+function searchCondition(){
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML='';
+
+    fetch('health_analysis.json').then(response => response.json()).then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition){
+            const symptoms = condition.symptoms.join(', ');
+            const prevention = condition.prevention.join(', ');
+            const treatment = condition.treatment;
+
+            resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+            resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+            resultDiv.innerHTML += `<p><strong>Symptoms:</strong>${symptoms}</p>`;
+            resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+            resultDiv.innerHTML += `<p><strong>Treatment:</strong>${treatment}</p>`;
+        }else{
+            resultDiv.innerHtml = 'Condition not found';
+        }
+    }).catch(error =>{
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+    })
 }
 
 function generateReport() {
